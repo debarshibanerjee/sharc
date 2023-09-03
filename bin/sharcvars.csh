89b408d1de8a79e49c39b1cae104b0408ed39b0e
Debarshi Banerjee <45908114+debarshibanerjee@users.noreply.github.com>
2023-09-03 19:00:06 +0200

    mula install

--- bin/sharcvars.csh
+++ bin/sharcvars.csh
@@ -1,6 +1,4 @@
-setenv SHARC=/global/mai/SHARC_3.0_tests/install/sharc/bin
-setenv PYSHARC=/global/mai/SHARC_3.0_tests/install/sharc/pysharc
-setenv SHARCLIB=/global/mai/SHARC_3.0_tests/install/sharc/lib
-setenv ANACONDA=/user/mai/anaconda/miniconda3/envs/pysharc_3.0_test
-setenv PYTHONPATH=$SHARCLIB:$PYSHARC:$PYTHONPATH
-setenv LD_LIBRARY_PATH=$SHARCLIB:$ANACONDA/lib:$LD_LIBRARY_PATH
+setenv SHARC=/media/users/debarshi/scratch/sharc/bin
+setenv SHARCLIB=/media/users/debarshi/scratch/sharc/lib
+setenv PYTHONPATH=$SHARCLIB:$PYTHONPATH
+setenv LD_LIBRARY_PATH=$SHARCLIB:$LD_LIBRARY_PATH
--- bin/sharcvars.sh
+++ bin/sharcvars.sh
@@ -1,6 +1,4 @@
-export SHARC=/global/mai/SHARC_3.0_tests/install/sharc/bin
-export PYSHARC=/global/mai/SHARC_3.0_tests/install/sharc/pysharc
-export SHARCLIB=/global/mai/SHARC_3.0_tests/install/sharc/lib
-export ANACONDA=/user/mai/anaconda/miniconda3/envs/pysharc_3.0_test
-export PYTHONPATH=$SHARCLIB:$PYSHARC:$PYTHONPATH
-export LD_LIBRARY_PATH=$SHARCLIB:$ANACONDA/lib:$LD_LIBRARY_PATH
+export SHARC=/media/users/debarshi/scratch/sharc/bin
+export SHARCLIB=/media/users/debarshi/scratch/sharc/lib
+export PYTHONPATH=$SHARCLIB:$PYTHONPATH
+export LD_LIBRARY_PATH=$SHARCLIB:$LD_LIBRARY_PATH
